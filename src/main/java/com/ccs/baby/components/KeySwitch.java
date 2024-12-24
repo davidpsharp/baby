edbe124d0e74e6fc46a8eaf31a30499ecb6d7dfc
Kyle Stevenson <kjstevenson@outlook.com>
2024-12-24 07:12:50 +0000

    Refactored and improved documentation for KeySwitch

--- src/main/java/com/ccs/baby/components/KeySwitch.java
+++ src/main/java/com/ccs/baby/components/KeySwitch.java
@@ -1,33 +1,40 @@
 package com.ccs.baby.components;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.AbstractButton;
+import java.awt.Insets;
+
 import com.ccs.baby.utils.ImageUtils;
 
-public class KeySwitch extends JButton
-{
+public class KeySwitch extends JButton {
 
-	public KeySwitch(String textValue, String downIconFileName, String upIconFileName)
-	{
-	
-		ImageIcon upIcon = ImageUtils.loadImage(upIconFileName,2400);
-		ImageIcon downIcon = ImageUtils.loadImage(downIconFileName, 2400);
-		
-		setIcon(upIcon);
-		setPressedIcon(downIcon);
-		
-		setFocusPainted(false);
-		setBorderPainted(false);
-		setContentAreaFilled(false);
-		
-		// top, left, bottom, right
-		Insets marginSpace = new Insets(3,3,3,3);
-		setMargin(marginSpace);
-		
-		setHorizontalTextPosition(AbstractButton.CENTER);
-		setVerticalTextPosition(AbstractButton.NORTH);
-		
-		setText(textValue);
-	}
+    private static final Insets DEFAULT_MARGIN = new Insets(3, 3, 3, 3);
+    private static final int DEFAULT_SCALE = 2400;
 
+    /**
+     * Constructs an InterlockingPushButton with the specified button text and vertical position for the text.
+     *
+     * @param buttonText       the text to be displayed on the button
+     * @param downIconFilePath the file path of the icon to be displayed when the button is pressed
+     * @param upIconFilePath   the file path of the icon to be displayed when the button is not pressed
+     */
+    public KeySwitch(String buttonText, String downIconFilePath, String upIconFilePath) {
+        ImageIcon upIcon = ImageUtils.loadImage(upIconFilePath, DEFAULT_SCALE);
+        ImageIcon downIcon = ImageUtils.loadImage(downIconFilePath, DEFAULT_SCALE);
+
+        setIcon(upIcon);
+        setPressedIcon(downIcon);
+
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+
+        setMargin(DEFAULT_MARGIN);
+
+        setHorizontalTextPosition(AbstractButton.CENTER);
+        setVerticalTextPosition(AbstractButton.NORTH);
+
+        setText(buttonText);
+    }
 }
