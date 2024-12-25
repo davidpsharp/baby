5c93c62056d527045a1e19ee506c32a227a522ed
Kyle Stevenson <kjstevenson@outlook.com>
2024-12-25 06:43:14 +0000

    Removed loadImage from Baby class

--- src/main/java/com/ccs/baby/core/Baby.java
+++ src/main/java/com/ccs/baby/core/Baby.java
@@ -322,32 +322,7 @@ public class Baby extends JFrame implements ActionListener
 
 	}
 	
-	private ImageIcon loadImage(String image) {
-      int MAX_IMAGE_SIZE = 2400;  //Change this to the size of
-                                 //your biggest image, in bytes.
-      int count = 0;
-      BufferedInputStream imgStream = new BufferedInputStream(
-                                    this.getClass().getResourceAsStream(image));
-      if (imgStream != null) {
-        byte buf[] = new byte[MAX_IMAGE_SIZE];
-        try {
-            count = imgStream.read(buf);
-            imgStream.close();
-        } catch (java.io.IOException ioe) {
-            System.err.println("Couldn't read stream from file: " + image);
-            return null;
-        }
-        if (count <= 0) {
-            System.err.println("Empty file: " + image);
-            return null;
-        }
-        return new ImageIcon(Toolkit.getDefaultToolkit().createImage(buf));
-      } 
-      else {
-        System.err.println("Couldn't find file: " + image);
-        return null;
-      }
-    }
+
 
 	// set up all the menus
 	public void menuSetup(JMenuBar m){
