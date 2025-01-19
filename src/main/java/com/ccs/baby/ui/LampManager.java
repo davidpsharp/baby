8f035f82ac11fc6d1794a7bc138fd9d4be742017
Kyle Stevenson <kjstevenson@outlook.com>
2025-01-19 10:11:31 +0000

    Minor JDoc update

--- src/main/java/com/ccs/baby/ui/LampManager.java
+++ src/main/java/com/ccs/baby/ui/LampManager.java
@@ -2,9 +2,12 @@ package com.ccs.baby.ui;
 
 import com.ccs.baby.core.Baby;
 
-// update the stop lamp according to the internally held stop flag
 public class LampManager {
 
+    /**
+     * Update the stop lamp according to the internally held stop flag.
+     * @param stopFlag the stop flag
+     */
     public void updateStopLamp(boolean stopFlag) {
         if (stopFlag) {
             Baby.mainPanel.setTexture(true);
