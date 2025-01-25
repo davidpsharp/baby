9c6e28f1f51f964540c50e528f64c038904f48ae
Kyle Stevenson <kjstevenson@outlook.com>
2025-01-25 22:24:10 +0000

    Refactor StaticisorPanel from SwitchPanel

--- src/main/java/com/ccs/baby/ui/StaticisorPanel.java
+++ src/main/java/com/ccs/baby/ui/StaticisorPanel.java
@@ -2,163 +2,191 @@ package com.ccs.baby.ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import com.ccs.baby.ui.components.ToggleSwitch;
-import com.ccs.baby.core.Control;
+import static com.ccs.baby.utils.CallbackUtils.runCallback;
 
 
 public class StaticisorPanel extends JPanel {
+    private Runnable onManAutoChange;
+    private Runnable onLineSwitchChange;
 
-    private ToggleSwitch manAuto;
+    private final ToggleSwitch manAuto;
+    private final ToggleSwitch[] lineSwitch = new ToggleSwitch[13];
+    private final ToggleSwitch[] functionSwitch = new ToggleSwitch[8];
 
-    public StaticisorPanel(Control control, Runnable onSwitchChange) {
+    public StaticisorPanel() {
 
-        GridBagLayout gb = new GridBagLayout();
-
-        setLayout(gb);
+        setLayout(new GridBagLayout());
         setOpaque(false);
-        setPreferredSize(new Dimension(300, 135));
+        setPreferredSize(new Dimension(300, 0));
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(0, 0, 0, 0);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.insets = new Insets(0, 0, 0, 0);
 
-        gbc.anchor = gbc.LINE_END;
-        gbc.gridwidth = 2;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
 
-
-        // L stat switches
         ToggleSwitch dud = new ToggleSwitch("Left unconnected.");
-        add(dud);
+        add(dud); // Needed to align ManAuto switch with the L stat switches
 
-        // set all unconnected L & F switches to all be down per Bob's comment that's what they do on the replica
+        initialiseLineSwitches(gridBagConstraints);  // L stat switches
+
+        gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.insets = new Insets(0, 0, 0, 0);
+
+        initialiseFunctionSwitches(gridBagConstraints); // F stat switches
+
+        // ManAuto switch
+        manAuto = new ToggleSwitch("Select whether to execute the store or the manual instruction.");
+        manAuto.addActionListener(e -> runCallback(onManAutoChange));
+        gridBagConstraints.gridx = 10;
+        add(manAuto, gridBagConstraints);
+
+        setManAuto(true); // Default to auto
+        setLineSwitches(true); // set L stat switches to all be on
+        setFunctionSwitches(true); // likewise F stat switches
+
+        //  set all unconnected L & F switches to all be down per Bob's comment that's what they do on the replica
         dud.setSelected(true);
+        lineSwitch[5].setSelected(true);
+        lineSwitch[6].setSelected(true);
+        lineSwitch[12].setSelected(true);
+        functionSwitch[3].setSelected(true);
+    }
+
+
+    public void setOnManAutoChange(Runnable callback) {
+        this.onManAutoChange = callback;
+    }
+
+    public void setOnLineSwitchChange(Runnable callback) {
+        this.onLineSwitchChange = callback;
+    }
+
+
+
+    private void initialiseLineSwitches(GridBagConstraints gridBagConstraints) {
 
         // SwitchLin Panel
         JPanel linePanel = new JPanel();
         linePanel.setOpaque(false);
-        ToggleSwitch[] lineSwitch = new ToggleSwitch[13];
-        ActionListener lineSwitchActionListener = new LineSwitchActionListener();
 
-        for (int switchNumber = 0; switchNumber < 13; switchNumber++) {
 
-            // Assign tooltips based on the switch number
-            if (switchNumber < 5) {
-                lineSwitch[switchNumber] = new ToggleSwitch("Selects the action line to be adjusted or executed.");
-            } else if (switchNumber == 5 || switchNumber == 6 || switchNumber == 12) {
-                lineSwitch[switchNumber] = new ToggleSwitch("Left unconnected.");
-                lineSwitch[switchNumber].setSelected(true);
+        for (int i = 0; i < 13; i++) {
+            if (i < 5) {
+                lineSwitch[i] = new ToggleSwitch("Selects the action line to be adjusted or executed.");
+            } else if (i == 5 || i == 6 || i == 12) {
+                lineSwitch[i] = new ToggleSwitch("Left unconnected.");
+                lineSwitch[i].setSelected(true);
             } else {
-                lineSwitch[switchNumber] = new ToggleSwitch(""); // No tooltip for other switches
+                lineSwitch[i] = new ToggleSwitch(""); // No tooltip for other switches
             }
 
             // Disable switches between 7 and 11
-            if (switchNumber > 6 && switchNumber < 12) {
-                lineSwitch[switchNumber].setEnabled(false);
+            if (i > 6 && i < 12) {
+                lineSwitch[i].setEnabled(false);
             }
 
             // Set GridBagConstraints and add to the panel
-            gbc.gridx = switchNumber + 1;
-            add(lineSwitch[switchNumber], gbc);
+            gridBagConstraints.gridx = i + 1;
+            add(lineSwitch[i], gridBagConstraints);
 
             // Add ActionListener
-            lineSwitch[switchNumber].addActionListener(lineSwitchActionListener);
+            lineSwitch[i].addActionListener(e -> runCallback(onLineSwitchChange));
         }
-
-        // Function Panel
-        JPanel functionPanel = new JPanel();
-        functionPanel.setOpaque(false);
-        ToggleSwitch[] functionSwitch = new ToggleSwitch[8];
-        for (int switchNumber = 0; switchNumber < 7; switchNumber++) {
-
-            if (switchNumber < 3) {
-                functionSwitch[switchNumber] = new ToggleSwitch("Selects the function number to be manually executed.");
-            } else if (switchNumber == 3) {
-                functionSwitch[switchNumber] = new ToggleSwitch("Left unconnected.");
-                functionSwitch[switchNumber].setSelected(true);
-            } else {
-                functionSwitch[switchNumber] = new ToggleSwitch("");
-                functionSwitch[switchNumber].setEnabled(false);
-            }
-
-            // Set GridBagConstraints and add to the panel
-            gbc.gridx = 3 + switchNumber;
-            gbc.anchor = gbc.CENTER;
-            add(functionSwitch[switchNumber], gbc);
-        }
-
-
-        manAuto = new ToggleSwitch("Select whether to execute the store or the manual instruction.");
-        manAuto.addActionListener(new ManAutoPressed());
-
-        add(manAuto, gbc);
-        gbc.gridx = 10;
-
-        // Default to auto
-        setManAuto(true);
-
-        // set L stat switches to all be on
-        for (int lStatSwitch = 0; lStatSwitch < 5; lStatSwitch++)
-            lineSwitch[lStatSwitch].setSelected(true);
-        // likewise F stat switches
-        for (int fStatSwitch = 0; fStatSwitch < 3; fStatSwitch++)
-            functionSwitch[fStatSwitch].setSelected(true);
-
     }
 
 
+    private void initialiseFunctionSwitches(GridBagConstraints gridBagConstraints) {
+        // Function Panel
+                JPanel functionPanel = new JPanel();
+                functionPanel.setOpaque(false);
+        for (int i = 0; i < 7; i++) {
 
-    // true is auto, false is man
+            if (i < 3) {
+                functionSwitch[i] = new ToggleSwitch("Selects the function number to be manually executed.");
+            }
+
+            if (i == 3) {
+                functionSwitch[i] = new ToggleSwitch("Left unconnected.");
+                functionSwitch[i].setSelected(true);
+            }
+
+            if (i > 3) {
+                functionSwitch[i] = new ToggleSwitch("Blah");
+                functionSwitch[i].setEnabled(false);
+                functionSwitch[i].setVisible(false);
+            }
+
+            // Set GridBagConstraints and add to the panel
+            gridBagConstraints.gridx = i + 3;
+            gridBagConstraints.anchor = GridBagConstraints.CENTER;
+            add(functionSwitch[i], gridBagConstraints);
+        }
+    }
+
+    // set L stat switches to all be on
+    public void setLineSwitches(boolean value) {
+        for (int i = 0; i < 5; i++) {
+            lineSwitch[i].setSelected(value);
+        }
+    }
+
+    // likewise F stat switches
+    public void setFunctionSwitches(boolean value) {
+        for (int i = 0; i < 5; i++) {
+            functionSwitch[i].setSelected(value);
+        }
+    }
+
+    /**
+     * Get the value of the manAuto switch
+     * TRUE = AUTO
+     * FALSE = MANUAL
+     *
+     * @return boolean value of the manAuto switch
+     */
     public boolean getManAuto() {
         return manAuto.isSelected();
     }
 
-    // true is auto, false is man
+    /**
+     * Set the value of the manAuto switch
+     * TRUE = AUTO
+     * FALSE = MANUAL
+     *
+     * @param value boolean value of the manAuto switch
+     */
     public void setManAuto(boolean value) {
         manAuto.setSelected(value);
     }
 
-
-    private class LineSwitchActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Call the method to update the action line
-            updateActionLine();
-        }
-    }
-
-    class ManAutoPressed implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            updateActionLine();
-        }
-    }
-
-    // return executable value of the line and function switches
-    // (as would be represented if taken from store)
-    public synchronized int getLineAndFunctionValue() {
-        int value = getLineValue() | (getFunctionValue() << 13);
-        return value;
-    }
-
-    // go through all line switches and return int of line selected
+    /**
+     * Iterate through all line switches and return the value of the line selected
+     *
+     * @return int value of line selected
+     */
     public int getLineValue() {
         int result = 0;
-        for (int switchNumber = 0; switchNumber < 5; switchNumber++) {
-            if (lineSwitch[switchNumber].isSelected())
-                result += (1 << switchNumber);
+        for (int i = 0; i < 5; i++) {
+            if (lineSwitch[i].isSelected()) result += (1 << i);
         }
         return result;
     }
 
-    // go through all function switches and return int of function selected
+    /**
+     * Iterate through all function switches and return the value of the function selected
+     *
+     * @return int value of function selected
+     */
     public int getFunctionValue() {
         int result = 0;
-        for (int switchNumber = 0; switchNumber < 3; switchNumber++) {
-            if (functionSwitch[switchNumber].isSelected())
-                result += (1 << switchNumber);
+        for (int i = 0; i < 3; i++) {
+            if (functionSwitch[i].isSelected()) result += (1 << i);
         }
         return result;
     }
