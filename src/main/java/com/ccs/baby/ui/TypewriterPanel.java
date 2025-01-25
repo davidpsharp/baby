5521c46ebdd547e37bf777d9e949d4ac46b6b842
Kyle Stevenson <kjstevenson@outlook.com>
2025-01-25 22:24:19 +0000

    Refactor TypewritePanel from SwitchPanel

--- src/main/java/com/ccs/baby/ui/TypewriterPanel.java
+++ src/main/java/com/ccs/baby/ui/TypewriterPanel.java
@@ -2,111 +2,90 @@ package com.ccs.baby.ui;
 
 import javax.swing.*;
 
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import java.awt.event.MouseListener;
-
-import com.ccs.baby.core.Baby;
 import com.ccs.baby.ui.components.PushButton;
-import com.ccs.baby.core.Store;
-import com.ccs.baby.core.Control;
+
+import static com.ccs.baby.utils.CallbackUtils.runCallback;
 
 public class TypewriterPanel extends JPanel {
 
-    public TypewriterPanel(Store aStore, Control aControl, SwitchPanel aSwitchPanel) {
+    // 32 keys plus 8 unconnected
+    public int CONNECTED_KEYS = 32;
+    public int UNCONNECTED_KEYS = 8;
+    public int TOTAL_KEYS = CONNECTED_KEYS + UNCONNECTED_KEYS;
 
+    private final Runnable[] onKeyPressedCallbacks;
+    private final Runnable[] onKeyReleasedCallbacks;
+
+    private final PushButton[] numberKeys;
+
+    // 8 (across) x 5 (down) grid
+    public TypewriterPanel() {
+
+        setLayout(new GridBagLayout());
+        setPreferredSize(new Dimension(0, 250));
         setOpaque(false);
-        //setBackground(backgroundColor);
-        setLayout(new GridBagLayout());   // 8 across. 5 down
 
+        numberKeys = new PushButton[TOTAL_KEYS];
+        onKeyPressedCallbacks = new Runnable[TOTAL_KEYS];
+        onKeyReleasedCallbacks = new Runnable[TOTAL_KEYS];
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.CENTER;
-        // gbc.weightx = 2;
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.fill = GridBagConstraints.CENTER;
 
-        PushButton[] numberKeys = new PushButton[40];
+        // Initialize and layout keys
+        initialiseNumberKeys(gridBagConstraints);
+    }
 
-        // create 40 keys
-        for (int keyNumber = 0; keyNumber < 40; keyNumber++) {
-            numberKeys[keyNumber] = new PushButton("", AbstractButton.CENTER);
+    private void initialiseNumberKeys(GridBagConstraints gridBagConstraints) {
+        for (int i = 0; i < TOTAL_KEYS; i++) {
 
             // add mouse listeners to keys 0-31
-            if (keyNumber < 32) {
-                numberKeys[keyNumber].addMouseListener(createTypewriterListener(keyNumber, aStore, aControl, aSwitchPanel));
-            }
+            if (i < CONNECTED_KEYS) {
+                numberKeys[i] = new PushButton("Typewriter adjusts bit " + i + " of the action line.");
 
+                int keyNumber = i; // Necessary for lambda
+                numberKeys[i].addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+                        runCallback(onKeyPressedCallbacks[keyNumber]);
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+                        runCallback(onKeyReleasedCallbacks[keyNumber]);
+                    }
+                });
+
+            } else {
+                numberKeys[i] = new PushButton("Left unconnected.");
+            }
         }
 
         // add keys in correct order for display with this layout
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 40; j += 5) {
-                gbc.gridx = j / 5;
-                gbc.gridy = i;
-                add(numberKeys[i + j], gbc);
+            for (int j = 0; j < TOTAL_KEYS; j += 5) {
+                gridBagConstraints.gridx = j / 5;
+                gridBagConstraints.gridy = i;
+                add(numberKeys[i + j], gridBagConstraints);
             }
-            gbc.anchor = gbc.LINE_END;
+            gridBagConstraints.anchor = GridBagConstraints.LINE_END;
         }
-
-        for (int keyNumber = 0; keyNumber < 32; keyNumber++) {
-            numberKeys[keyNumber].setToolTipText("Typewriter adjusts bit " + keyNumber + " of the action line.");
-        }
-        for (int keyNumber = 32; keyNumber < 40; keyNumber++) {
-            numberKeys[keyNumber].setToolTipText("Left unconnected.");
-        }
-
-        //setBorder( BorderFactory.createLoweredBevelBorder() );
-        //setBorder( BorderFactory.createLoweredBevelBorder() );
-        setPreferredSize(new Dimension(0, 270));
-
     }
 
-
-    // typewriter button pushed
-    private MouseListener createTypewriterListener(int aKeyNumber, Store aStore, Control aControl, SwitchPanel aSwitchPanel) {
-        return new MouseListener() {
-            @Override
-            public void mousePressed(java.awt.event.MouseEvent e) {
-                // if running then adjust every action line
-                if (Baby.running) {
-                    // signal control that a key is pressed
-                    // arguments: key pressed, number of key pressed, is it a write?
-                    aControl.setKeyPressed(true, aKeyNumber);
-
-                    // note, m1sim incorrectly stops running in this case rather than corrupting store lines
-                }
-                // otherwise just do current action line
-                else {
-                    int lineNumber = aSwitchPanel.getLineValue();
-                    // true = write, false = erase
-                    if (aSwitchPanel.getEraseWrite()) {
-                        aStore.setLine(lineNumber, aStore.getLine(lineNumber) | (1 << aKeyNumber));
-                    } else {
-                        aStore.setLine(lineNumber, aStore.getLine(lineNumber) & (~(1 << aKeyNumber)));
-                    }
-                    aSwitchPanel.redrawCrtPanel();
-                }
-            }
-
-            @Override
-            public void mouseReleased(java.awt.event.MouseEvent e) {
-                // notify that the key is no longer pressed whether Baby is still
-                // running or not.
-                aControl.setKeyPressed(false, aKeyNumber);
-            }
-
-            @Override
-            public void mouseEntered(java.awt.event.MouseEvent e) {
-            }
-
-            @Override
-            public void mouseExited(java.awt.event.MouseEvent e) {
-            }
-
-            @Override
-            public void mouseClicked(java.awt.event.MouseEvent e) {
-            }
-        };
+    public void setOnKeyPressed(int i, Runnable callback) {
+        if (i >= 0 && i < onKeyPressedCallbacks.length) {
+            onKeyPressedCallbacks[i] = callback;
+        }
     }
+
+    public void setOnKeyReleased(int i, Runnable callback) {
+        if (i >= 0 && i < onKeyReleasedCallbacks.length) {
+            onKeyReleasedCallbacks[i] = callback;
+        }
+    }
+
 }
