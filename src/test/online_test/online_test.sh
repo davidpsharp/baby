071a79d77ab8026ce413735b3667d1a046ce1afd
davidpsharp <misc@davidsharp.com>
2025-01-29 15:20:11 +0000

    minor fix & comments in local webserver launch script

--- src/test/online_test/online_test.sh
+++ src/test/online_test/online_test.sh
@@ -1,9 +1,13 @@
 
-# Mac OS shell script to copy HTML file to target, open web browser and launch a Python3 web server to test out cheerpj 
+# Mac OS shell script to copy HTML file to target, open web browser and launch a Python3 web server to test out cheerpj serving the root of target folder 
+# Run this script from current working directory of: baby/src/test/online_test 
 
+# install the index.html in target folder
 cp index.html ../../../target/index.html
 
+# launch the web browser which will retry to connect to the web server which we start in the next step
 open -a open -a Google\ Chrome http://localhost:8080
 
+# launch the web server, serving the root of the target folder
 python3 -m http.server 8080 -d ../../../target/
 
