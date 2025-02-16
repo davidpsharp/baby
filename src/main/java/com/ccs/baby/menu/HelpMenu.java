b6743a97f0992ce038197165fbd952a9db513c76
davidpsharp <misc@davidsharp.com>
2025-02-16 00:37:29 +0000

    About dialog now auto closes on cheerpj to workaround cheerpj bug

--- src/main/java/com/ccs/baby/menu/HelpMenu.java
+++ src/main/java/com/ccs/baby/menu/HelpMenu.java
@@ -3,46 +3,9 @@ package com.ccs.baby.menu;
 import com.ccs.baby.manual.ReferenceManual;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URI;
-
-import com.ccs.baby.utils.MiscUtils;
-import com.ccs.baby.utils.Version;
 
 public class HelpMenu {
-
-    private static final String ABOUT_TITLE = "Manchester Baby Simulator";
-    private static final String ABOUT_MESSAGE = String.join("<br>",
-            "Manchester Baby Simulator",
-            "",
-            "v" + Version.getVersion(),
-            (MiscUtils.getBuildTime() == null) ? "" : "Built " + MiscUtils.getBuildTime(),
-            "",
-            "Originally by <a href='https://davidsharp.com'>David Sharp</a>",
-            "January 2001",
-            "With thanks to Chris Burton for his consultation on historical matters.",
-            "The GUI was created from pictures of the Baby replica by Gulzaman Khan",
-            "August 2006",
-            "",
-            "<a href='https://manchesterbaby.com'>manchesterbaby.com</a>",
-            "<a href='https://github.com/davidpsharp/baby'>github.com/davidpsharp/baby</a>",
-            "",
-            "Licensed under the GPL 3.0",
-            "",
-            "Thanks to Leaning Technologies for providing the <a href='https://cheerpj.com/'>CheerpJ</a> JVM to run the online version." 
-    );
-
-    private static ImageIcon icon = new ImageIcon(HelpMenu.class.getResource("/icons/baby.png"));
-
-    /**
-     * Creates the Help menu.
-     *
-     * @param frame the frame object
-     * @return the Help menu
-     */
     public static JMenu createHelpMenu(JFrame frame) {
         // Create the Help menu
         JMenu helpMenu = new JMenu("Help");
@@ -68,32 +31,7 @@ public class HelpMenu {
     }
 
     private static void showAboutDialog(JFrame frame) {
-        JEditorPane editorPane = new JEditorPane();
-        editorPane.setContentType("text/html");
-        editorPane.setText("<html><body style='width: 300px; padding: 5px; font-family: Arial, sans-serif;'>" + ABOUT_MESSAGE + "</body></html>");
-        editorPane.setEditable(false);
-        editorPane.setBackground(UIManager.getColor("Panel.background"));
-        
-        // Make links clickable
-        editorPane.addHyperlinkListener(e -> {
-            if (e.getEventType() == javax.swing.event.HyperlinkEvent.EventType.ACTIVATED) {
-                try {
-                    Desktop.getDesktop().browse(new URI(e.getURL().toString()));
-                } catch (IOException | URISyntaxException ex) {
-                    JOptionPane.showMessageDialog(frame,
-                            "Could not open link: " + ex.getMessage(),
-                            "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-
-        JOptionPane.showMessageDialog(
-                frame.getContentPane(),
-                editorPane,
-                ABOUT_TITLE,
-                JOptionPane.INFORMATION_MESSAGE,
-                icon
-        );
+        AboutDialog dialog = new AboutDialog(frame);
+        dialog.displayAboutDialog();
     }
 }
